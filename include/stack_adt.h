84acc8882deb94a9c5c028eb5f0c2274ab5f384e
Teal <avengers.asembleee@gmail.com>
2022-06-22 16:02:32 -0400

    Added node struct definition to header file

--- include/stack_adt.h
+++ include/stack_adt.h
@@ -1,7 +1,13 @@
 #ifndef TDQC_PHASE_ONE_SRC_STACK_STACK_ADT_H_
 #define TDQC_PHASE_ONE_SRC_STACK_STACK_ADT_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <stdint.h>
+typedef struct node_t node_t;
 typedef struct stack_adt_t stack_adt_t;
 
 stack_adt_t * stack_init(uint32_t stack_size);
@@ -11,5 +17,7 @@ void stack_push(stack_adt_t * stack, void * node);
 void * stack_pop(stack_adt_t * stack);
 void stack_dump(stack_adt_t * stack);
 
-
+#ifdef __cplusplus
+}
+#endif
 #endif //TDQC_PHASE_ONE_SRC_STACK_STACK_ADT_H_
