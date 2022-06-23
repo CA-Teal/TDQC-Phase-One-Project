cf5dea16e76d87c109080fa41356126891716ac3
Teal <avengers.asembleee@gmail.com>
2022-06-22 21:13:38 -0400

    Modified stack_adt_t struct to include a single pointer array

--- include/stack_adt.h
+++ include/stack_adt.h
@@ -7,13 +7,23 @@ extern "C"
 #endif
 
 #include <stdint.h>
-typedef struct node_t node_t;
-typedef struct stack_adt_t stack_adt_t;
+#include <string.h>
+
+
+typedef struct node_t{
+    void * data;
+}node_t;
+
+typedef struct stack_adt_t{
+    uint32_t size;
+    uint32_t items;
+    node_t *stack_array;
+}stack_adt_t;
 
 stack_adt_t * stack_init(uint32_t stack_size);
 void stack_destroy(stack_adt_t * stack);
 void * stack_nth_peek(stack_adt_t * stack, uint32_t index);
-void stack_push(stack_adt_t * stack, void * node);
+void stack_push(stack_adt_t * stack, void * data);
 void * stack_pop(stack_adt_t * stack);
 void stack_dump(stack_adt_t * stack);
 
@@ -21,3 +31,4 @@ void stack_dump(stack_adt_t * stack);
 }
 #endif
 #endif //TDQC_PHASE_ONE_SRC_STACK_STACK_ADT_H_
+
