8ec92c4ab19331a92d3e842916c7040c6917e93f
Teal <avengers.asembleee@gmail.com>
2022-06-22 21:14:47 -0400

    Completed push and pop funtions

--- src/stack/stack_adt.c
+++ src/stack/stack_adt.c
@@ -2,36 +2,44 @@
 #include <stack_adt.h>
 #include <stdio.h>
 
-typedef struct node_t {
-    void * data;
-}node_t;
-
-typedef struct stack_adt_t{
-    uint size;
-    uint items;
-    node_t ** stack_array;
-}stack_adt_t;
-
 
 stack_adt_t * stack_init(uint32_t stack_size){
     stack_adt_t * new_stack = (stack_adt_t *)calloc(1, sizeof(stack_adt_t));
     new_stack->size = stack_size;
     new_stack->items = 0;
-    new_stack->stack_array = (node_t **)calloc(stack_size, sizeof(*(new_stack->stack_array)));
+    new_stack->stack_array = (node_t*)calloc(stack_size, sizeof(*(new_stack->stack_array)));
     return new_stack;
 }
 
 
-void stack_push(stack_adt_t * stack, void * node){
+void stack_push(stack_adt_t * stack, void * data){
     if (stack->items == stack->size)
     {
-        fprint(stderr, "No more items can currently be pushed to the stack");
+        fprintf(stderr, "No more items can currently be pushed to the stack");
         return;
     }
-
+    
     uint current = stack->items;
-    stack->stack_array[current] = node;
+    stack->stack_array[current].data = calloc(1, sizeof(void*));
+
+    memcpy(stack->stack_array[current].data, data, sizeof(void*));
     stack->items++;
+}
+
+void * stack_pop(stack_adt_t * stack){
+    if (NULL == stack)
+    {
+        fprintf(stderr, "The stack contains no more items");
+        return NULL;
+    }
+
+    int top_node = stack->items - 1;
+    void * pop_data = calloc(1, sizeof(void*));
+    memcpy(pop_data, stack->stack_array[top_node].data, sizeof(void*));
+
+    free(stack->stack_array[top_node].data);
+    stack->items--;
+    return pop_data;
 }
 
 void * stack_nth_peek(stack_adt_t * stack, uint32_t index){
@@ -40,11 +48,6 @@ void * stack_nth_peek(stack_adt_t * stack, uint32_t index){
 
 }
 
-void * stack_pop(stack_adt_t * stack){
-    void * ptr = (void *)3;
-    return ptr;
-
-}
 
 void stack_dump(stack_adt_t * stack){
 
@@ -55,10 +58,13 @@ void stack_destroy(stack_adt_t * stack){
     {
         return;
     }
-    
-    for(int i = 0; i < stack->items; i++){
-        free(stack->stack_array[i]);
+    for (size_t i = 0; i < stack->items; i++)
+    {
+        free(stack->stack_array[i].data);
     }
+    
+    free(stack->stack_array);
     free(stack);
+    stack = NULL;
     stack = NULL;
 }
