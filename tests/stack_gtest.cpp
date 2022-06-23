b2498fb63e057f51f1685c687da0978f6cde1b02
Teal <avengers.asembleee@gmail.com>
2022-06-22 21:16:02 -0400

    Added tests for stack_init, push, and pop functions

--- tests/stack_gtest.cpp
+++ tests/stack_gtest.cpp
@@ -1,7 +1,32 @@
 #include <gtest/gtest.h>
 #include <stack_adt.h>
 
-TEST(BaseTest, BaseTestOne)
+
+TEST(BaseTest, CreateStack)
 {
-    EXPECT_EQ(1, 3);
+    stack_adt_t * new_stack = stack_init(5);
+    EXPECT_NE(new_stack, nullptr);
+    stack_destroy(new_stack);
+}
+
+TEST(BaseTest, PushNode)
+{
+    stack_adt_t * new_stack = stack_init(5);
+    int data = 67;
+    int data2 = 89;
+    stack_push(new_stack, &data);
+    stack_push(new_stack, &data2);
+    EXPECT_EQ(new_stack->items , 2);
+    EXPECT_EQ(*(int*)new_stack->stack_array[0].data, data);
+    stack_destroy(new_stack);
+}
+
+TEST(BaseTest, PopNode)
+{
+    stack_adt_t * new_stack = stack_init(5);
+    int data = 67;
+    stack_push(new_stack, &data);
+    void * data_catch = stack_pop(new_stack);
+    EXPECT_EQ(*(int*)data_catch, data);
+    stack_destroy(new_stack);
 }
