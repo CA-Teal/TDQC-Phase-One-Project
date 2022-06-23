248df5bf865bd8eddec44bc421b37e9f3bd8354b
Teal <avengers.asembleee@gmail.com>
2022-06-23 10:23:39 -0400

    Created test for nth_seek function

--- tests/stack_gtest.cpp
+++ tests/stack_gtest.cpp
@@ -29,4 +29,24 @@ TEST(BaseTest, PopNode)
     void * data_catch = stack_pop(new_stack);
     EXPECT_EQ(*(int*)data_catch, data);
     stack_destroy(new_stack);
+    free(data_catch);
+}
+
+TEST(BaseTest, SeekNode)
+{
+    stack_adt_t * new_stack = stack_init(5);
+    int data = 67;
+    int data1 = 58; 
+    int data2 = 3;
+    stack_push(new_stack, &data);
+    stack_push(new_stack, &data1);
+    stack_push(new_stack, &data2);
+    void * peek_info = stack_nth_peek(new_stack, 2);
+    if(NULL != peek_info){
+        EXPECT_EQ(*(int*)peek_info, data2);
+    }else{
+        EXPECT_EQ(peek_info, nullptr);
+    }
+    stack_destroy(new_stack);
+    free(peek_info);
 }
